e3d0b215856894fb39b8c8cb2a49dd033794d388
ShaKQl <halimovsvhzod@gmail.com>
2023-08-02 00:11:26 +0500

    task5

---
+++ Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.ConstrainedExecution;
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number < 0) number *= -1; //number = number * -1
+if (number >= 100 && number <= 999)
+{
+    int lastDigit = number % 10;
+    Console.Write($"Последняя цифра числа: {lastDigit}");
+}
+else
+{
+    Console.Write("Неверное число.");
+}
