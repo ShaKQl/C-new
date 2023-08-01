7d5968e1722ef34e1fc3a320d4c691ee887400d3
ShaKQl <halimovsvhzod@gmail.com>
2023-08-01 23:57:13 +0500

    task4

---
+++ Task04/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Ввведите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+
+if (count < 0){
+    while (count <= number){
+        Console.Write($"  |{count}|  ");
+        count++;
+    }
+}
+
+else{
+    while (count >= number){
+        Console.Write($"  |{count}|  ");
+        count--;
+    }
+}
